bc37c8b64a4ad5560eef580d6699581058b206d9
veekthor <victorilozulu@gmail.com>
2020-01-07 19:56:25 +0100

    - remove Joi import - imported mongoose - connected to mongodb

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const Joi = require('joi');
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
+
+//connect to Mongo DB
+mongoose.connect('mongodb://localhost/vidly')
+            .then(()=> console.log('Connected to MongoDB ....'))
+                .catch((err) => console.log('Error', err));
+
 
 //middle ware
 app.use(express.json());
