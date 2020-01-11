7cd900e98e9acc359f865ca9e23f3b048f9c332e
veekthor <victorilozulu@gmail.com>
2020-01-11 15:47:56 +0100

    Set maxlength property in genreSchema

--- modules/genres.js
+++ modules/genres.js
@@ -5,7 +5,8 @@ const genreSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        minlength: 3
+        minlength: 3,
+        maxlength: 50
     }
 });
 
