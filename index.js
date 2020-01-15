65857322f9376304a701a03658f42df19554b768
veekthor <victorilozulu@gmail.com>
2020-01-15 20:50:10 +0100

    Implemented error logging to MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('express-async-errors');
 const winston = require('winston');
+require('winston-mongodb');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -17,6 +18,11 @@ const app = express();
 
 //configure winston to log to file
 winston.add(winston.transports.File, {filename: 'logfile.log'});
+//configure winston to log to MongoDB
+winston.add(winston.transports.MongoDB, { 
+    db: 'mongodb://localhost/vidly',
+    level: 'error'
+});
 
 if(!config.get('jwtPrivateKey')){
     console.log('FATAL ERROR: Jwt private key not set');
