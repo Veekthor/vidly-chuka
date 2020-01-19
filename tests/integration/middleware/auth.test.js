4a8c3112043664801909da9bc9eb940aa662c840
veekthor <victorilozulu@gmail.com>
2020-01-19 22:09:48 +0100

    Implemented authorization middleware Integration test

---
+++ tests/integration/middleware/auth.test.js
@@ -0,0 +1,44 @@
+const request = require('supertest');
+const { Genre } = require('../../../models/genres');
+const { User } = require('../../../models/users');
+
+describe('Authorization middleware', () => {
+    beforeEach(() => {server = require('../../../index')});
+    afterEach( async () => { 
+        server.close()
+        await Genre.remove({}); // cleans up DB after test
+    });
+
+    let token;
+    //Happy Path
+    const exec = () => {
+        return request(server)
+                .post('/api/genres')
+                .set('x-auth-token', token)
+                .send({ name: 'genre1'}) //send valid data so that error is from auth
+    }
+
+    it('should return 400 if token is invalid', async () => {
+        token = '1234';
+        
+        const res = await exec();
+
+        expect(res.status).toBe(400);
+    });
+
+    it('should return 401 if token is not provided', async () => {
+        token = '';
+        
+        const res = await exec();
+
+        expect(res.status).toBe(401);
+    });
+
+    it('should return 200 if token is valid', async () => {
+        token = new User().generateAuthToken();
+        
+        const res = await exec();
+
+        expect(res.status).toBe(200);
+    });
+});
