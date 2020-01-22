a6ae3c9beab0953c747a12f17fa56c6a96f5d586
veekthor <victorilozulu@gmail.com>
2020-01-22 17:28:36 +0100

    Developed returns route using TDD

--- routes/returns.js
+++ routes/returns.js
@@ -1,5 +1,7 @@
+const moment = require('moment');
 const auth = require('../middleware/auth');
 const {Rental} = require('../models/rental');
+const { Movie } = require('../models/movies');
 const express = require('express');
 const router = express.Router();
 
@@ -12,6 +14,20 @@ router.post('/', auth, async (req, res, next) => {
         'movie._id': req.body.movieId 
     });
     if(!rental) return res.status(404).send('No rental found');
+    
+    if(rental.dateReturned) return res.status(400).send('rental already processed');
+
+    rental.dateReturned = new Date();
+    const diffDays = moment().diff(rental.dateOut, 'days'); //calculate the days difference
+    rental.rentalFee = diffDays * rental.movie.dailyRentalRate;
+    await rental.save();
+    await Movie.update({_id: req.body.movieId},{
+        $inc: {numberInStock: 1}
+    });
+
+
+    
+    res.status(200).send(rental);
 });
 
 module.exports = router;
--- tests/integration/routes/returns.test.js
+++ tests/integration/routes/returns.test.js
@@ -1,14 +1,17 @@
 const request = require('supertest');
 const { Rental } = require('../../../models/rental');
 const { User } = require('../../../models/users');
+const { Movie } = require('../../../models/movies');
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 describe('/api/returns', () => {
     let server,
     customerId,
     movieId,
     rental,
-    token;
+    token,
+    movie;
 
     //Happy path
     const exec = () =>{
@@ -36,11 +39,22 @@ describe('/api/returns', () => {
                 dailyRentalRate: 2
             }
         });
+
+        movie = new Movie({
+            _id: movieId,
+            title: '12345',
+            dailyRentalRate: 2,
+            genre: {name: '12345'},
+            numberInStock: 10
+        });
+        
+        await movie.save();
         await rental.save();
     });
     afterEach( async () => { 
-       await server.close();
+        await server.close();
         await Rental.remove({}); // cleans up DB after test
+        await Movie.remove({});
     });
 
     it('should return 401 if user is not logged in', async () => {
@@ -67,7 +81,59 @@ describe('/api/returns', () => {
 
     it('should return 404 if no rental found with customerId/movieId', async () => {
         await Rental.remove({});
+
         const res = await exec();
+        
         expect(res.status).toBe(404);
+    });
+
+    it('should return 400 if rental is already processed', async () => {
+        rental.dateReturned = new Date();
+
+        await rental.save();
+
+        const res = await exec();
+        
+        expect(res.status).toBe(400);
+    });
+
+    it('should set movie returned date if input is valid', async () => {
+        const res = await exec();
+        
+        const rentalInDB = await Rental.findById(rental._id);
+        const diff = new Date() - rentalInDB.dateReturned;
+        expect(diff).toBeLessThan(10 * 1000);
+    });
+
+    it('should set rentalFee if input is valid', async () => {
+        //Set date out to 7 days before
+        rental.dateOut = moment().add(-7, 'days').toDate();
+        await rental.save();
+
+        const res = await exec();
+        
+        const rentalInDB = await Rental.findById(rental._id);
+        
+        expect(rentalInDB.rentalFee).toBe(14);
+    });
+
+    it('should increase movie stock', async () => {
+        
+        const res = await exec();
+        
+        const movieInDB = await Movie.findById(movie._id);
+        
+        expect(movieInDB.numberInStock).toBe(movie.numberInStock + 1);
+    });
+
+    it('should return the rental if inout is valid', async () => {
+        
+        const res = await exec();
+        
+        const rentalInDB = await Rental.findById(rental._id);
+
+        expect(Object.keys(res.body)).toEqual(
+            expect.arrayContaining(['dateOut', 'dateReturned', 'rentalFee', 'customer', 'movie']
+            ));
     });
 });
