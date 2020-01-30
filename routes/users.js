32e09c7cfefddcb8bae88f57a40f048c62d4fbb0
veekthor <victorilozulu@gmail.com>
2020-01-30 22:43:09 +0100

    Added @swagger comment for POST

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,35 @@ router.get('/me', auth, async (req, res) =>{
     res.send(user);
 });
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: Users management
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /api/users:
+ *    post:
+ *      summary: Create/Register a new user
+ *      tags: [Users]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *      responses:
+ *        "200":
+ *          description: A user schema
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ */
+
 router.post('/', validate(validateUserDetails), async (req, res) =>{
     let user = await User.findOne({ email: req.body.email }); // Check if user exists
     if (user) return res.status(400).send('user already registered');
