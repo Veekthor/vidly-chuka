94bbda7fa40d32ffb758c31c9ace8196dd5cd077
veekthor <victorilozulu@gmail.com>
2020-02-02 23:57:25 +0100

    added config validation for db string

--- startup/config.js
+++ startup/config.js
@@ -1,7 +1,12 @@
 const config = require('config');
 
 module.exports = function () {
+    //checks if jwtPrivateKey & db connection string are set 
     if(!config.get('jwtPrivateKey')){
-    throw new Error('FATAL ERROR: Jwt private key not set');
+        throw new Error('FATAL ERROR: Jwt private key not set');
+    }
+
+    if(!config.get('db')){
+        throw new Error('FATAL ERROR: DB connection string not set');
     }
 }
