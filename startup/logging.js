26d4c385dfdb8d416e945ac05a15b8348c08369e
veekthor <victorilozulu@gmail.com>
2020-02-02 23:23:17 +0100

    added comments and  winston db logging

--- startup/logging.js
+++ startup/logging.js
@@ -1,22 +1,24 @@
 const winston = require('winston');
-// require('winston-mongodb');
+const config = require('config')
+require('winston-mongodb');
 require('express-async-errors');
 
-module.exports = function(){//Handle Uncaught exception
+module.exports = function(){
+    //Handle Uncaught exception
     winston.handleExceptions(
         new winston.transports.Console({ colorize: true, prettyPrint: true}),
-        new winston.transports.File({filename: 'uncaughtExceptions.log'})
+        new winston.transports.File({filename: 'uncaughtExceptions.log'}) //saves to file
     );
-    //Handle Unhandled Promise Rejection
+    //Handle Unhandled Promise Rejection and rethrow as exception
     process.on('unhandledRejection', (ex) =>{
         throw ex;
     });
 
     //configure winston to log to file
     winston.add(winston.transports.File, {filename: 'logfile.log'});
-    //configure winston to log to MongoDB
-    // winston.add(winston.transports.MongoDB, { 
-    //     db: 'mongodb://localhost/vidly',
-    //     level: 'error'
-    // });
+    // configure winston to log to MongoDB
+    winston.add(winston.transports.MongoDB, { 
+        db: config.get('db'),
+        level: 'error'
+    });
 }
