df38c23fbda62fa56abd1eaf9a46e5e7cea0ea75
veekthor <victorilozulu@gmail.com>
2020-02-02 23:56:32 +0100

    handled exception

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,15 +3,16 @@ const config = require('config');
 
 //Custom middleware function
 module.exports = function (req, res, next){ 
-    const token = req.header('x-auth-token');// get token
+    const token = req.header('x-auth-token');// read token from request header
     if (!token) return res.status(401).send('Access denied. Token not provided');// check if token was sent
 
     try{
-    const decode = jwt.verify(token, config.get('jwtPrivateKey')); //verify/decode token
-    req.user = decode;
+    const decode = jwt.verify(token, config.get('jwtPrivateKey')); //verify & decode token
+    req.user = decode; // sets req.user to jwt payload
     next(); //move handler to next middleware
     }
     catch(ex){
         res.status(400).send('Invalid token.');
+        throw ex;
     }
 }
